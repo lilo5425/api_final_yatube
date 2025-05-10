34a57a926c038133cd84049860cb567c06b1b243
Елизавета Архипова <yelizaveta.arkhipova.2005@mail.ru>
2025-05-10 21:20:07 +0700

    Bug fixed №6

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,20 +1,17 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from rest_framework_nested.routers import NestedSimpleRouter
-
 from .views import PostViewSet, GroupViewSet, CommentViewSet, FollowViewSet
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='posts')
 router.register(r'groups', GroupViewSet, basename='groups')
 router.register(r'follow', FollowViewSet, basename='follow')
-
-posts_router = NestedSimpleRouter(router, r'posts', lookup='post')
-posts_router.register(r'comments', CommentViewSet, basename='post-comments')
+router.register(r'^posts/(?P<post_pk>\d+)/comments', CommentViewSet,
+                basename='comments'
+                )
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/', include(posts_router.urls)),
     path('v1/', include('djoser.urls')),
     path('v1/', include('djoser.urls.jwt')),
 ]
