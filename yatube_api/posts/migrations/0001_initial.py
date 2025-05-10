3205f081dd8232ed59ddb2c4983c72f53ee2f92e
Елизавета Архипова <yelizaveta.arkhipova.2005@mail.ru>
2025-05-10 18:10:50 +0700

    Update migrations

--- yatube_api/posts/migrations/0001_initial.py
+++ yatube_api/posts/migrations/0001_initial.py
@@ -13,6 +13,16 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Group',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=200)),
+                ('slug', models.SlugField(unique=True)),
+                ('description', models.TextField()),
+            ],
+        ),
+        
+        migrations.CreateModel(
             name='Post',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
@@ -20,8 +30,10 @@ class Migration(migrations.Migration):
                 ('pub_date', models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')),
                 ('image', models.ImageField(blank=True, null=True, upload_to='posts/')),
                 ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='posts', to=settings.AUTH_USER_MODEL)),
+                ('group', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='posts', to='posts.group')),
             ],
         ),
+        
         migrations.CreateModel(
             name='Comment',
             fields=[
@@ -31,5 +43,19 @@ class Migration(migrations.Migration):
                 ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='comments', to=settings.AUTH_USER_MODEL)),
                 ('post', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='comments', to='posts.post')),
             ],
+        ),
+        
+        migrations.CreateModel(
+            name='Follow',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('following', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='following', to=settings.AUTH_USER_MODEL)),
+                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='follower', to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        
+        migrations.AddConstraint(
+            model_name='follow',
+            constraint=models.UniqueConstraint(fields=('user', 'following'), name='unique_follow'),
         ),
     ]
