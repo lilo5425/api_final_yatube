34bbbd0a248c81892241df146d22c2db33a299fd
Елизавета Архипова <yelizaveta.arkhipova.2005@mail.ru>
2025-05-10 21:48:13 +0700

    Bug Fixed №8

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,18 +1,21 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            TokenRefreshView)
 
 from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 
-router = DefaultRouter()
-router.register(r'posts', PostViewSet)
-router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet, 'post')
-router.register(r'group', GroupViewSet)
-router.register(r'follow', FollowViewSet)
+
+router_v1 = DefaultRouter()
+router_v1.register(r'posts', PostViewSet,
+                   basename='posts')
+router_v1.register(r'groups', GroupViewSet, basename='groups')
+router_v1.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet,
+                   basename='comments')
+router_v1.register(r'follow', FollowViewSet,
+                   basename='follow')
+
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('', include(router.urls)),
+    path('v1/', include(router_v1.urls)),
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
 ]
